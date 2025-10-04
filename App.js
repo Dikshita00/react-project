5e4a17b498ed59c57085ea9e23feb3640ba128e2
Dikshita Singh <159521918+Dikshita00@users.noreply.github.com>
2025-10-04 13:16:54 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginDemo from "./Login";
-import Home from "./Home";
+import React from 'react';
+import DemoCallBack from './WithCallback'; 
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginDemo />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <DemoCallBack />
+    </div>
   );
 }
 
